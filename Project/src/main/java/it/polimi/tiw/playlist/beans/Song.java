8f1b96f5968d536531a6a313b5aa1777071d6dac
Mattia Campana <campana.mattia@outlook.com>
2023-04-19 17:56:06 +0200

    Implemented all Beans

--- Project/src/main/java/it/polimi/tiw/playlist/beans/Song.java
+++ Project/src/main/java/it/polimi/tiw/playlist/beans/Song.java
@@ -2,5 +2,71 @@ package it.polimi.tiw.playlist.beans;
 
 
 public class Song{
-	
+	private int id;
+    private String title;
+    private String genre;
+    private String fileAudio;
+    private String user;
+    private Album album;
+
+    public Song(){
+        this.id = 0;
+        this.title = null;
+        this.genre = null;
+        this.fileAudio = null;
+        this.user = null;
+        this.album = null;
+    }
+
+    public Song(int id, String title, String genre, String fileAudio, String user, Album album){
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.fileAudio = fileAudio;
+        this.user = user;
+        this.album = album;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getId(){
+        return this.id;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setFileAudio(String fileAudio) {
+        this.fileAudio = fileAudio;
+    }
+    public String getFileAudio() {
+        return fileAudio;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+    public String getUser() {
+        return user;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
+    }
+    public Album getAlbum() {
+        return album;
+    }
+    
 }
--- Project/src/main/java/it/polimi/tiw/playlist/beans/User.java
+++ Project/src/main/java/it/polimi/tiw/playlist/beans/User.java
@@ -2,5 +2,30 @@ package it.polimi.tiw.playlist.beans;
 
 
 public class User{
-	
+	private String userName;
+    private String password;
+
+    public User(){
+        this.userName= null;
+        this.password= null;
+    }
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setUsernName(String userName) {
+        this.userName = userName;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
