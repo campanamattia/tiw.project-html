6063e9f3701877218b49b7b896b80b9a4b313b5d
FraRita <francescorita2709@gmail.com>
2023-04-24 16:46:33 +0200

    created PlaylistServlet

---
+++ Project/src/main/java/it/polimi/tiw/playlist/controller/PlaylistServlet.java
@@ -0,0 +1,110 @@
+package it.polimi.tiw.playlist.controller;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import java.sql.*;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import it.polimi.tiw.playlist.dao.SongDAO;
+import it.polimi.tiw.playlist.beans.Song;
+import it.polimi.tiw.playlist.utils.ConnectionHandler;
+import it.polimi.tiw.playlist.utils.TemplateHandler;
+import java.util.ArrayList;
+
+public class PlaylistServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	private Connection connection;
+	private TemplateEngine templateEngine;
+	
+	public PlaylistServlet() {
+		super();
+	}
+	
+	public void init() throws ServletException{
+		try {
+			ServletContext context = getServletContext();
+			this.connection = ConnectionHandler.getConnection(context);
+			this.templateEngine = TemplateHandler.getTemplateEngine(context);
+			
+		} catch (UnavailableException  e) {
+			
+		}
+	}
+	
+	//this page must be accessible only by selecting a playlist in the home page,
+	//by the buttons in the playlist page
+	// or through a doPost of EditPlaylistServlet (using ForwardPlaylist).
+	//Doing so, the parameters in the session have been already checked
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+		HttpSession session = request.getSession(true);
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		String userName = (String)session.getAttribute("user");
+		
+		//taking attributes from session
+		String playlistName = (String)session.getAttribute("playlistName");
+		String playlistError = (String)session.getAttribute("playlistError");
+		int lowerBound = session.getAttribute("lowerBound") != null ? (Integer)session.getAttribute("lowerBound") : 0;
+		
+		//removing attributes from the session because they identify that the page has been loaded in the intended way 
+		session.removeAttribute("playlistName");
+		session.removeAttribute("playlistError");
+		session.removeAttribute("lowerBound");
+		
+		//taking all the user's songs contained in the playlist
+		ArrayList<Song> allSongs = null;
+		String error = null;
+		try {
+			allSongs = new SongDAO(this.connection).getSongTitleAndImg(playlistName, userName);
+		}
+		catch(SQLException e) {
+			error = "Database error: Unable to load your playlist";
+		}
+		if(error != null) {
+			session.setAttribute("generalError", error);
+			String path = servletContext.getContextPath() + "/Home";
+			response.sendRedirect(path);
+			return;
+		}
+		
+		//taking only the ones to render on the page
+		ArrayList<Song> songs = new ArrayList<Song>();
+		for(int i=0; i<5 && error == null; i++) {
+			try {
+				songs.add(allSongs.get(lowerBound + i));
+			}
+			catch(IndexOutOfBoundsException e) {
+				error = "Section not found";
+			}
+		}
+		
+		if(songs.isEmpty()) {
+			session.setAttribute("generalError", error);
+			String path = servletContext.getContextPath() + "/Home";
+			response.sendRedirect(path);
+			return;
+		}
+		
+		//starting to prepare the presentation of the page
+		ctx.setVariable("playlistName", playlistName);
+		if(playlistError != null) ctx.setVariable("playlistError", playlistError);
+		ctx.setVariable("songs", songs);
+		ctx.setVariable("lowerBound", lowerBound);
+		if(lowerBound == 0) ctx.setVariable("previousButton", 0);
+		else  ctx.setVariable("previousButton", 1);
+		if(lowerBound + 5 < allSongs.size()) ctx.setVariable("nextButton", 1);
+		else ctx.setVariable("nextButton", 0);
+		
+		templateEngine.process("/WEB-INF/playlist.html", ctx, response.getWriter());	
+	}
+		
+	public void destroy() {
+	      ConnectionHandler.destroy(this.connection);
+	}
+	
+}
