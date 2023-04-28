4f46d1a7a6ffb18497a75601c36e4a073a1ea426
FraRita <francescorita2709@gmail.com>
2023-04-28 19:06:17 +0200

    GetImageServlet finished

--- Project/src/main/java/it/polimi/tiw/playlist/controller/GetImageServlet.java
+++ Project/src/main/java/it/polimi/tiw/playlist/controller/GetImageServlet.java
@@ -1,8 +1,8 @@
 package it.polimi.tiw.playlist.controller;
 
 import java.io.*;
+import java.net.URLDecoder;
 import java.nio.file.Files;
-import java.nio.file.Path;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -37,8 +37,44 @@ public class GetImageServlet extends HttpServlet {
 	
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+		HttpSession session = request.getSession(true);
+		ServletContext servletContext = getServletContext();
+		String userName = (String)session.getAttribute("user");
 		
+		//Take the pathInfo from the request
+		String pathInfo = request.getPathInfo();
 		
+		//Check if the path info is valid
+		if (pathInfo == null || pathInfo.equals("/")) return;
+		
+		//Take the fileName from the pathInfo without the "/" character
+		String fileImage = URLDecoder.decode(pathInfo.substring(1), "UTF-8");
+		
+		//check if the file image belongs to the user
+		try {
+			if( !(new SongDAO(this.connection).imgBelongTo(fileImage, userName)) ) return;
+		}catch(SQLException e) {
+			return;
+		}
+		
+		//Open the file
+		String fileName = userName + "_" + fileImage;
+		File file = new File(imgFolderPath, fileName);
+		
+		if (!file.exists() || file.isDirectory()) {
+			return;
+		}
+		
+		//Set headers for browser
+		response.setHeader("Content-Type", servletContext.getMimeType(fileName));
+		response.setHeader("Content-Length", String.valueOf(file.length()));
+		
+		//inline     -> the user will watch the image immediately
+		//filename   -> used to indicate a fileName if the user wants to save the file
+		response.setHeader("Content-Disposition", "inline; filename=\"" + file.getName() + "\"");
+		
+		//Copy the file to the output stream
+		Files.copy(file.toPath(), response.getOutputStream());		
 	}
 	
 	public void destroy() {
