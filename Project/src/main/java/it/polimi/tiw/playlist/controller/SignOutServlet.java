a2400cf84d216d952f8b030b72f69cefd80c5069
FraRita <francescorita2709@gmail.com>
2023-05-01 16:59:01 +0200

    Created GetSongServlet and SignOutServlet

---
+++ Project/src/main/java/it/polimi/tiw/playlist/controller/SignOutServlet.java
@@ -0,0 +1,27 @@
+package it.polimi.tiw.playlist.controller;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+public class SignOutServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	public SignOutServlet() {
+		super();
+	}
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+		//Invalidate session
+		request.getSession(true).invalidate();;
+		
+		//Redirect to the sign in page
+		String path = getServletContext().getContextPath() +  "/SignIn";
+		response.sendRedirect(path);
+
+	}
+	
+	
+}
